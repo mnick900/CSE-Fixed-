218d3e67f5c722254182404a6e9309ff32b4ebe4
mnick900 <54081135+mnick900@users.noreply.github.com>
2019-10-30 12:48:43 -0700

    My third commit

--- notes.py
+++ notes.py
@@ -1,2 +1,24 @@
+# This is a comment
 print("Hello World")
 print("NSM")
+print("this is a string")
+print("123abc")  # yet another comment
+print('12')  # apostrophe works too!
+print("123a")
+
+# printing numbers
+print(7)  # this is an integer
+print(3.14159265359)  # I'm a float
+print(-3.14159265359)
+
+# printing booleans
+print(True)
+print(False)
+
+"""
+this is a 
+multi-line
+comment
+"""
+# error catching
+    
