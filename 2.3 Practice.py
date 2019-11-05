330030ee6fc879932f983e99d819b61f9b7915d4
mnick900 <54081135+mnick900@users.noreply.github.com>
2019-11-05 12:55:20 -0800

    Math

---
+++ 2.3 Practice.py
@@ -0,0 +1,22 @@
+x = 89.5
+y = 300.7
+mean = x+y
+mean /= 2
+print(mean)
+
+print()
+
+r = 5
+h = 10
+volume = 3.14 * r**2 * h
+print(volume)
+
+
+
+
+
+
+
+
+
+
--- notes.py
+++ notes.py
@@ -89,3 +89,4 @@ print(5 == 5)
 print(7 == 5)
 print(5 != 5)
 print(7 != 5)
+
