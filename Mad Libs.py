ed53bce328bfdeda148a8abd481635edbb73a172
mnick900 <54081135+mnick900@users.noreply.github.com>
2020-01-09 12:31:21 -0800

    i fixed things

--- Mad Libs.py
+++ Mad Libs.py
@@ -1,19 +1,19 @@
 # coding=utf-8
-ML1 = input("Type of family member").upper()
-ML2 = input("Age").upper()
-ML4 = input("Profession").upper()
-ML5 = input("Adjective").upper()
-ML6 = input("Color").upper()
-ML7 = input("Unit of time (plural)").upper()
-ML8 = input("Part of a house").upper()
-ML9 = input("Temperature descriptor").upper()
-ML10 = input("Place").upper()
-ML12 = input("Object").upper()
-her = str("her")
-print(""" Our {} to-be thief was on a {} with her {} and her 
+ML1 = str(input("Type of family member").upper())
+ML2 = str(input("Age").upper())
+ML4 = str(input("Profession").upper())
+ML5 = str(input("Adjective").upper())
+ML6 = str(input("Color").upper())
+ML7 = str(input("Unit of time (plural)").upper())
+ML8 = str(input("Part of a house").upper())
+ML9 = str(input("Temperature descriptor").upper())
+ML10 = str(input("Place").upper())
+ML12 = str(input("Object").upper())
+
+print(''' Our {} to-be thief was on a {} with her {} and her 
 brother, still blissfully unaware. Her {} and brother sat in front of her, and her brother was 
 having a coughing fit courtesy of his cold. All of the sudden her brother stopped coughing and fell silent. The
- {} book thief became worried and went to feel the head of the brother, it was stone cold...
+{} book thief became worried and went to feel the head of the brother, it was stone cold...
 
 As the impromptu funeral ceremony was coming to an end the {} to-be thief spotted a glimpse of {}
 among the snow by the {}. As everyone started to hurry out of the {}, the book thief grabbed what
@@ -25,5 +25,7 @@ Buried beneath the folded layer of clothes in that {} was a small {} book, which
 a {}-year-old {} in town had probably spent the last few {}
 looking for. “I promise you,” he will say to his boss, “I have no idea what happened to it. I’ve looked
 everywhere. Everywhere!” I’m sure he would never have suspected the {} thief, and yet, there it was—a 
-{} book with {} words written against the {} of """, her, """
-clothes.""".format(ML5, ML10, ML1, ML1, ML5, ML5, ML6, ML4, ML9, ML1, ML6, ML1, ML10, ML12, ML6, ML2, ML4, ML7, ML5, ML6, ML6, ML8))
+{} book with {} words written against the {} of her
+clothes.''' .format(ML2, ML10, ML1, ML1, ML5, ML5, ML6, ML10, ML9, ML1, ML1, ML6, ML1, ML10, ML12, ML6, ML2, ML4, ML7,
+                    ML5,
+                    ML6, ML6, ML8,))
