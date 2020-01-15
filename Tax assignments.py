fbf7e7a82a797622699de06df6605d0d4e31d1c4
mnick900 <54081135+mnick900@users.noreply.github.com>
2020-01-15 12:24:40 -0800

    Quest 2

--- Tax assignments.py
+++ Tax assignments.py
@@ -1,11 +1,22 @@
+"""
 order_amount = int(input("How many items are you getting"))
 item_price = int(input("How much is the item you are getting?"))
 state = str(input("What state do you live in?"))
 tax = 1
-if state == "Ca" or 'CA' or 'ca' or 'cA' or "California":
-    tax = 1.0875
-    cost = (order_amount * item_price) * tax
+cost = (order_amount * item_price)
+if state == "Ca" or state == 'CA' or state == 'ca' or state == 'cA' or state == "California":
+    tax = 1.08
+    cost *= tax
     print("The total cost is ${:.2f}".format(cost))
+"""
+print("The password for the user person1 is 090909")
+user = input()
+pas = "090909"
+entered = input("What is the password for {}".format(user))
+if entered == pas:
+    print("Welcome")
+else:
+    print("Sorry, that is the wrong password")
 
 
 
