693b2d33f6cd2d056494707506680a7190ae3aff
mnick900 <54081135+mnick900@users.noreply.github.com>
2020-02-04 13:02:18 -0800

    1 minute left aaaaaaaaaaaaaaaaaaaaa

---
+++ Guess game.py
@@ -0,0 +1,93 @@
+import random
+type = input("Do you want to manually change what the bounds are?\n Answer with(y/n)").upper()
+while type != "Y" and type != "N":
+    type = input("Do you want to manually change what the bounds are?\n Answer with(y/n)").upper()
+if type == "Y":
+    up = int(input("What do you want the upper bound to be?"))
+    low = int(input("What do you want the lower bound to be?"))
+    num = random.randint(low, up)
+    print(num)
+    guess = int(input("Guess a number from {} - {}").format(low, up))
+    gum = 0
+    for i in range(4):
+        if guess < num:
+            print("Guess higher")
+            gum = gum + 1
+            guess = int(input("Guess a number from {} - {}").format(low, up))
+        elif guess > num:
+            print("Guess lower")
+            gum = gum + 1
+            guess = int(input("Guess a number from {} - {}").format(low, up))
+if type == "N":
+    mode = input("Type EASY if you wish to play easy, or HARD if you wish to play hard")
+    while mode != "HARD" and mode != "EASY":
+        mode = input("Type EASY if you wish to play easy, or HARD if you wish to play hard")
+    if mode == "EASY":
+        num = random.randint(1,10)
+        print(num)
+        guess = int(input("Guess a number from 1-10"))
+        gum = 0
+        for i in range (4):
+            if guess < num:
+                print("Guess higher")
+                gum = gum + 1
+                guess = int(input("Guess a number from 1-10"))
+            elif guess > num:
+                print("Guess lower")
+                gum = gum + 1
+                guess = int(input("Guess a number from 1-10"))
+    if mode == "HARD":
+        num = random.randint(1,100)
+        print(num)
+        guess = int(input("Guess a number from 1-100"))
+        gum = 0
+        for i in range (4):
+            if guess < num:
+                print("Guess higher")
+                gum = gum + 1
+                guess = int(input("Guess a number from 1-100"))
+            elif guess > num:
+                print("Guess lower")
+                gum = gum + 1
+                guess = int(input("Guess a number from 1-100"))
+    if gum == 4:
+        print("That's too many tries")
+    if gum < 4:
+        print("You did it, good job!")
+    print("It took you ",gum + 1,"tries to answers correct.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- While loops exercises.py
+++ While loops exercises.py
@@ -24,27 +24,33 @@ while pass1 != passs:
         print("Wrong")
         pass1 = input("What is your password")
 print("That's it")
+"""
 # 4
 color = input("color guess time").upper()
+s = 0
 while color != "GREEN":
     color = input("Color guess time").upper()
     if color == "BLUE":
         print("Add yellow")
+        s = s + 1
     elif color == "YELLOW":
         print("Add Blue")
+        s = s + 1
     elif color == "RED":
         print("Add other primaries")
+        s = s + 1
     else:
         print("Nope")
 print("güt")
-"""
+print("You used {} tries".format(s))
 
+"""
 num = int(input("Enter a number from 1 - 10"))
-while num < 1 and num > 10:
+while num < 1 or num > 10:
     print("It isn't hard")
     num = int(input("Enter a number from 1 - 10"))
-
-
+print("Correct")
+"""
 
 
 
