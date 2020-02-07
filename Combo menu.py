0e8f4daacd609f744e186dc853be0f832a58be72
mnick900 <54081135+mnick900@users.noreply.github.com>
2020-02-07 14:02:11 -0800

    e

--- Combo menu.py
+++ Combo menu.py
@@ -7,16 +7,19 @@ c3 = 0
 c4 = 0
 bev = "n"
 fri = "n"
+kat = 'y'
+kats = 0
 sandt = input("What kind of sandwich do you want? \n Chicken $5.25 \n Beef $6.25 \n Tofu $5.75").upper()
 while sandt != "CHICKEN" and sandt != "BEEF" and sandt != "TOFU":
     print("That is not a valid option.")
     sandt = input("What kind of sandwich do you want? \n Chicken $5.25 \n Beef $6.25 \n Tofu $5.75").upper()
-    if sandt == "CHICKEN":
+if sandt == "CHICKEN":
         c1 = 5.25
-    elif sandt == "BEEF":
+elif sandt == "BEEF":
         c1 = 6.25
-    elif sandt == "TOFU":
+elif sandt == "TOFU":
         c1 = 5.75
+
 bev = input("Would you like a beverage? Answer with (y/n)").upper()
 while bev != "Y" and bev != "N":
     bev = input("Would you like a beverage? Answer with (y/n)").upper()
@@ -30,6 +33,7 @@ if bev == "Y":
         c2 = 1.75
     elif bevs == "LARGE":
         c2 = 2.25
+
 fri = input("Would you like fries? Answer with (y/n)").lower()
 while fri != "y" and fri != "n":
     fri = input("Would you like fries? Answer with (y/n)").lower()
@@ -44,17 +48,28 @@ if fri == "y":
             mega = input("Would you like to mega-size your fries? Answer with (y/n)").lower()
         if mega == "y":
             c3 = 2.00
-        fris = "MEGA"
+            fris = "MEGA"
     elif fris == "medium":
         c3 = 1.50
     elif fris == "large":
         c3 = 2.00
+
+kat = input("Do you want ketchup? Answer with (y/n)").lower()
+while kat != "y" and kat != "n":
+    kat = input("Do you want ketchup? Answer with (y/n)").lower()
+if kat == "y":
+    kats = int(input("How many ketchup packets do you want? \n 1 Ketchup is $0.25"))
+    while kats < 1:
+        kats = int(input("How many ketchup packets do you want? \n 1 Ketchup is $0.25"))
+    c4 = .25 * kats
 
 print("You chose {} for your sandwich".format(sandt))
 if bev == "Y":
     print("You chose the {} size for your drink.".format(bevs))
 if fri == "y":
     print("You chose the {} size for your fries.".format(fris))
+if kat == "y":
+    print("You got {} ketchup packets".format(kats))
 cost = c1 + c2 + c3 + c4
 print("The cost is ${:.2f}".format(cost))
 
